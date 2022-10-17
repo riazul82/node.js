fff847c45640e4696424b430293b2c04cc7f8725
riazul islam <riazulislam5280@gmail.com>
2022-10-17 16:50:14 +0600

    local module

---
+++ local-module/app.js
@@ -0,0 +1,8 @@
+const Math = require('./math');
+
+let res1 = Math.sum(10, 20); // 30
+let res2 = Math.sub(20, 10); // 10
+let res3 = Math.mul(10, 20); // 200
+let res4 = Math.div(20, 10); // 2
+
+console.log(res1, res2, res3, res4);
